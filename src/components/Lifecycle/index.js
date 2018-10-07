e077277b3513da08e07745475a37fd6c0b61739a
Ricalyn Haboc <ricalyn.g.haboc@accenture.com>
2018-10-08 02:33:58 +0800

    Updates lifecycle methods

--- src/components/Lifecycle/index.js
+++ src/components/Lifecycle/index.js
@@ -17,7 +17,24 @@ export default class LifeCycle extends Component {
   componentWillUnmount() {
     // do cleanup here before component destroy: like action listener.
   }
+
+  shouldComponentUpdate() {
+    // You can access `this.props` and `this.state` here
+    // This function should return a boolean, whether the component should re-render.
+    return false;
+  }
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    // Do not call this.setState here as it will cause re-render
+    if (this.props.message !== prevProps.message) {
+      //do something here
+    }
+  }
+
   render() {
+    // You can access `this.props` and `this.state` here
+    // Do not call this.setState here
     return (
       <div />
     )
