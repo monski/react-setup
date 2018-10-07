c786092e3b1ff048b0101fceeee724cfc9a7bac4
monski <mr.mon21@gmail.com>
2018-10-07 10:48:05 +0800

    remove unused

--- src/middleware.js
+++ src/middleware.js
@@ -12,29 +12,15 @@ const promiseMiddleware = store => next => action => {
   if (isPromise(action.payload)) {
     store.dispatch({ type: ASYNC_START, subtype: action.type });
 
-    const currentView = store.getState().viewChangeCounter;
-    const skipTracking = action.skipTracking;
-
     action.payload.then(
       res => {
-        const currentState = store.getState()
-        if (!skipTracking && currentState.viewChangeCounter !== currentView) {
-          return
-        }
         action.payload = res;
         store.dispatch({ type: ASYNC_END, promise: action.payload });
         store.dispatch(action);
       },
       error => {
-        const currentState = store.getState()
-        if (!skipTracking && currentState.viewChangeCounter !== currentView) {
-          return
-        }
         action.error = true;
         action.payload = error.response.body;
-        if (!action.skipTracking) {
-          store.dispatch({ type: ASYNC_END, promise: action.payload });
-        }
         store.dispatch(action);
       }
     );
--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger'
+import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createHashHistory';
 import { promiseMiddleware } from './middleware';
 import reducer from './reducer';
-
-import { routerMiddleware } from 'react-router-redux'
-import createHistory from 'history/createHashHistory';
 
 
 export const history = createHistory();
@@ -13,12 +12,13 @@ export const history = createHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
 const myRouterMiddleware = routerMiddleware(history);
+
 const getMiddleware = () => {
   if (process.env.NODE_ENV === 'production') {
     return applyMiddleware(myRouterMiddleware, promiseMiddleware);
   }
   // Enable additional logging in non-production environments.
-  return applyMiddleware(myRouterMiddleware, promiseMiddleware, createLogger({collapsed: true}))
+  return applyMiddleware(myRouterMiddleware, promiseMiddleware, createLogger({collapsed: true}));
 };
 
 export const store = createStore(reducer, composeWithDevTools(getMiddleware()));
